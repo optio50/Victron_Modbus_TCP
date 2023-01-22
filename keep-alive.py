6b7a34c4b4dbf12f3cb27406976352a21daedc37
optio50 <optio50@mail.com>
2023-01-22 14:39:41 -0900

    Add files via upload
    keep-alive files

---
+++ keep-alive.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import paho.mqtt.publish as mqttpublish
+from time import sleep
+
+# GX Device I.P Address
+ip = '192.168.20.156'
+
+#===================================
+
+# VRM Portal ID from GX device. 
+# Menu -->> Settings -->> "VRM Online Portal -->> VRM Portal ID"
+VRMid = "d41243d31a90"
+
+#===================================
+mqttpublish.single("R/"+VRMid+"/system/0/Serial", hostname=ip, port=1883)
+
+# "Keep-Alive" MQTT request
+while True:
+    mqttpublish.single("R/"+VRMid+"/system/0/Serial", hostname=ip, port=1883)
+    print("Keep Alive Sent")
+    sleep(10)
